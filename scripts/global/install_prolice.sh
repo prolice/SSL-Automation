684c8c6ab4d3674ff8943475a7b89856b7ea5212
Christophe Van Beneden <48445910+prolice@users.noreply.github.com>
2020-12-09 22:48:24 +0100

    Plus besoin

--- scripts/global/install_prolice.sh
+++ scripts/global/install_prolice.sh
@@ -6,10 +6,6 @@ echo 'Veuillez renseigner le chemin d accès à votre dossier data'
 read -p 'Foundry Data Path=' -e -i '.local/share/FoundryVTT/Config' foundrydata
 echo 'foundrydata='$foundrydata >> foundryssl/variables.sh
 
-echo 'Veuilez entrer votre nom de registrar : <registrar.domain.com>'
-read domain_registrar
-echo 'domain_registrar='$domain_registrar >> foundryssl/variables.sh
-
 echo 'Veuillez entrer votre FQDN (Full Qualified Domain Name) : <domain.com>'
 read fqdn
 echo 'fqdn='$fqdn >> foundryssl/variables.sh
